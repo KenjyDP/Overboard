070f7ad80d869cbc04b62b7249934644a1f75e15
Kenjy Depoorter <kenjy.depoorter@student.howest.be>
2024-04-30 12:16:45 +0200

    Placeholder MainScreen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        Debug.Log("PlayButton has been pressed.");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
